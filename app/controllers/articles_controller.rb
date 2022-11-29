c47a6a91443303c07e5b90d1cbc3c42cd7024ae5
Mason Holland <mason_holland@taylor.edu>
2022-11-28 19:39:05 -0500

    MVC commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -12,7 +15,7 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
    
     if @article.save
       redirect_to @article
@@ -21,8 +24,29 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to root_path, status: :see_other
+  end
+
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
